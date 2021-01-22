67d36c2fb487f84ac7f4415e6380e1385fddcf01
maxgw <maxgw2@yahoo.co.uk>
2021-01-22 18:59:07 +0000

    Initial commit, including spring boot zip.

---
+++ src/main/kotlin/max/kotlin/springboot/demo/KotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package max.kotlin.springboot.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDemoApplication>(*args)
+}
---
+++ src/test/kotlin/max/kotlin/springboot/demo/KotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package max.kotlin.springboot.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
